3a0ac1b8643835b655afc25689e397d3fcad4dc6
Zac Johnston <zac.johnston@monash.edu>
2020-02-20 11:40:03 -0500

    [config.py] created config.py

--- progs/__init__.py
+++ progs/__init__.py
@@ -1,8 +1,10 @@
 from . import prog
 from . import paths
 from . import load_save
+from . import config
 
 __all__ = ['prog',
            'paths',
            'load_save',
+           'config',
            ]
