2f85a898ab5e0b862d332b08aa7e40e8ebe100d0
Zac Johnston <zac.johnston@monash.edu>
2020-02-20 16:46:04 -0500

    [analysis.py] created analysis.py

--- progs/__init__.py
+++ progs/__init__.py
@@ -3,10 +3,12 @@ from . import paths
 from . import load_save
 from . import configuration
 from . import strings
+from . import analysis
 
 __all__ = ['prog',
            'paths',
            'load_save',
            'configuration',
            'strings',
+           'analysis',
            ]
