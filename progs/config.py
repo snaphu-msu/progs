faad4adbacba1103dd574ccfe812128cbbfb885e
Zac Johnston <zac.johnston@monash.edu>
2020-02-20 14:11:07 -0500

    [config.py] added function: load_config()

---
+++ progs/config.py
@@ -0,0 +1,37 @@
+import configparser
+import os
+import ast
+
+# progs
+from . import paths
+from .strings import printv
+
+"""
+Module for handling configs for each progenitor series
+"""
+
+
+def load_config(series, verbose=True):
+    """Load .ini config file and return as dict
+
+    parameters
+    ----------
+    series : str
+    verbose : bool
+    """
+    filepath = paths.config_filepath(series)
+    printv(f'Loading config: {filepath}', verbose)
+
+    if not os.path.exists(filepath):
+        raise FileNotFoundError(f'Config file not found: {filepath}')
+
+    ini = configparser.ConfigParser()
+    ini.read(filepath)
+
+    config = {}
+    for section in ini.sections():
+        config[section] = {}
+        for option in ini.options(section):
+            config[section][option] = ast.literal_eval(ini.get(section, option))
+
+    return config
