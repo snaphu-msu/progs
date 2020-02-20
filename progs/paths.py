c9f333551ab44a98e9e67e112c6c05308ab5a578
Zac Johnston <zac.johnston@monash.edu>
2020-02-19 20:00:36 -0500

    [paths.py] created

---
+++ progs/paths.py
@@ -0,0 +1,29 @@
+import os
+
+path = '~/projects/data/progenitors'
+
+aliases = {
+    's16': 'sukhbol_2016',
+    's18': 'sukhbol_2018',
+}
+
+
+def get_prog_filename(mass, series):
+    """Return filename of progenitor
+
+    parameters
+    ----------
+    mass : float/int
+    series : str
+    """
+    pass
+
+
+def check_alias(series):
+    """Return full name of series if alias used
+
+    parameters
+    ----------
+    series : str
+    """
+    pass
