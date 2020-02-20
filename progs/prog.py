40980cfe0b45283227eab8e1f0056b99dfd57106
Zac Johnston <zac.johnston@monash.edu>
2020-02-20 16:02:09 -0500

    [prog.py] updated docstring

--- progs/prog.py
+++ progs/prog.py
@@ -3,6 +3,10 @@ from . import paths
 from . import load_save
 from . import configuration
 
+"""
+Class for handling a given progenitor model
+"""
+
 
 class Prog:
     def __init__(self, mass, series, verbose=True):
@@ -18,6 +22,23 @@ class Prog:
         series : str
             Name of progenitor series/set, e.g. 'sukhbold_2016'.
             Shorthand aliases may be defined, e.g. 's16' for 'sukhbold_2016'.
+        verbose : bool
+
+
+        attributes
+        ----------
+        mass : int or float
+        series : str
+        verbose : bool
+        filename : str
+        filepath : str
+            Path to raw progenitor file
+        config : dict
+            Progenitor-specific parameters loaded from 'config/[series].ini'
+        table : pd.DataFrame
+            Main table of radial profile parameters, including composition
+        composition : pd.DataFrame
+            subset of table containing only network species abundances (mass fraction)
         """
         self.mass = mass
         self.series = paths.check_alias(series)
