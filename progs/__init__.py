8e331953863b2109db41c792c66ebbeb38b98af5
Zac Johnston <zac.johnston@monash.edu>
2020-02-20 14:12:10 -0500

    [__init__.py] updated

--- progs/__init__.py
+++ progs/__init__.py
@@ -2,9 +2,11 @@ from . import prog
 from . import paths
 from . import load_save
 from . import config
+from . import strings
 
 __all__ = ['prog',
            'paths',
            'load_save',
            'config',
+           'strings',
            ]
