7273ff121e43ad4217c097892a3a5eaf58e54619
Zac Johnston <zac.johnston@monash.edu>
2020-02-21 15:52:24 -0500

    [network.py] added function: get_sums()

--- progs/network.py
+++ progs/network.py
@@ -1,5 +1,5 @@
+import numpy as np
 import pandas as pd
-import os
 
 # progs
 from . import paths
@@ -9,13 +9,40 @@ Functions for handling nuclear network information
 """
 
 
-def load_net(network):
+def load_net(network_name):
     """Load network table from file
 
     parameters
     ----------
-    network : str
+    network_name : str
         name of network, e.g. 'net19'
     """
-    filepath = paths.network_filepath(network)
+    filepath = paths.network_filepath(network_name)
     return pd.read_csv(filepath, delim_whitespace=True)
+
+
+def get_sums(composition, network):
+    """Calculate summed quantities from isotope composition
+
+    parameters
+    ----------
+    composition : pd.DataFrame
+        table of isotope mass fractions
+    network : pd.DataFrame
+        table of isotopes to sum over, as returned by load_net().
+        isotope labels must match the column names in `composition`
+    """
+    out = {}
+    n_zones = len(composition)
+
+    for key in ['sumx', 'sumy', 'ye']:
+        out[key] = np.zeros(n_zones)
+
+    for row in network.itertuples():
+        x_i = np.array(composition[row.isotope])
+
+        out['sumx'] += x_i
+        out['sumy'] += x_i / row.A
+        out['ye'] += x_i * (row.Z / row.A)
+
+    return out
