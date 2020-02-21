5a26321b94dee63f7b8ed6fcffa8c5c88e001c68
Zac Johnston <zac.johnston@monash.edu>
2020-02-21 16:51:30 -0500

    [plotting.py] added function: check_ax()

--- progs/plotting.py
+++ progs/plotting.py
@@ -28,3 +28,23 @@ def setup_subplots(n_sub, max_cols=2, sub_figsize=(6, 5), **kwargs):
     figsize = (n_cols*sub_figsize[0], n_rows*sub_figsize[1])
 
     return plt.subplots(n_rows, n_cols, figsize=figsize, **kwargs)
+
+
+def check_ax(ax, figsize):
+    """Setup fig, ax if ax is not provided
+
+    parameters
+    ----------
+    ax : Axes
+    figsize : [width, height]
+    """
+    fig = None
+
+    if ax is None:
+        fig, ax = plt.subplots(figsize=figsize)
+
+    return fig, ax
+
+
+# TODO:
+#   - Migrating others from flashbang.Simulation
