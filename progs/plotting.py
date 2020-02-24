4513eb07b228b1bc6988ce546a1e2909741c4c43
Zac Johnston <zac.johnston@monash.edu>
2020-02-24 16:44:49 -0500

    [plotting.py] added functions from flashbang: set_ax_lims(), set_ax_legend()

--- progs/plotting.py
+++ progs/plotting.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 """
 General functions for plotting
 """
@@ -25,7 +24,7 @@ def setup_subplots(n_sub, max_cols=2, sub_figsize=(6, 5), **kwargs):
     """
     n_rows = int(np.ceil(n_sub / max_cols))
     n_cols = {False: 1, True: max_cols}.get(n_sub > 1)
-    figsize = (n_cols*sub_figsize[0], n_rows*sub_figsize[1])
+    figsize = (n_cols * sub_figsize[0], n_rows * sub_figsize[1])
 
     return plt.subplots(n_rows, n_cols, figsize=figsize, **kwargs)
 
@@ -46,5 +45,63 @@ def check_ax(ax, figsize):
     return fig, ax
 
 
-# TODO:
-#   - Migrating others from flashbang.Simulation
+# def set_ax_scales(ax, y_var, x_var, y_scale, x_scale):
+#     """Set axis scales (linear, log)
+#
+#     parameters
+#     ----------
+#     ax : Axes
+#     y_var : str
+#     x_var : str
+#     y_scale : {'log', 'linear'}
+#     x_scale : {'log', 'linear'}
+#     """
+#     if x_scale is None:
+#         x_scale = self.config['plotting']['ax_scales'].get(x_var, 'log')
+#     if y_scale is None:
+#         y_scale = self.config['plotting']['ax_scales'].get(y_var, 'log')
+#
+#     ax.set_xscale(x_scale)
+#     ax.set_yscale(y_scale)
+
+
+# def _set_ax_title(ax, chk, title):
+#     """Set axis title
+#
+#     parameters
+#     ----------
+#     ax : Axes
+#     chk : int
+#     title : bool
+#     """
+#     if title:
+#         timestep = dt * chk - self.bounce_time
+#         ax.set_title(f't = {timestep:.3f} s')
+
+
+def set_ax_lims(ax, xlims=None, ylims=None):
+    """Set x and y axis limits
+
+    parameters
+    ----------
+    ax : Axes
+    xlims : [min, max]
+    ylims : [min, max]
+    """
+    if ylims is not None:
+        ax.set_ylim(ylims)
+    if xlims is not None:
+        ax.set_xlim(xlims)
+
+
+def set_ax_legend(ax, legend, loc=None):
+    """Set axis labels
+
+    parameters
+    ----------
+    ax : Axes
+    legend : bool
+    loc : str or int
+    """
+    if legend:
+        ax.legend(loc=loc)
